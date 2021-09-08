d4c6ae576736fced8a91ab581eadbfb7b5e4c46f
Ben-Hur Jeffer <67138155+BenHurJF@users.noreply.github.com>
2021-09-08 01:14:13 -0300

    Ultima-Att

--- cypress/integration/CadastrarUsuario/steps.js
+++ cypress/integration/CadastrarUsuario/steps.js
@@ -1,7 +1,7 @@
 /// <reference types="cypress" />
 import CadastrarUsuario from '../../support/pagesObjects/Cadastrar/index.js';
 
-// CADASTRAR USUÁRIO
+// CADASTRAR USUÁRIO - VALIDAR USUÁRIO - EXCLUIR USUÁRIO
 
 When(/^Eu acionar a opção cadastrar e os campos estiverem preenchidos com dados válidos, sistema exibe abaixo do formulário uma tabela com os dados do usuário informados anteriormente e a opção Excluir.$/, () => {
 	CadastrarUsuario.cadastrarUsuario();
